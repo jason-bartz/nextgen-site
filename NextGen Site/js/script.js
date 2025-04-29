d8a379e0be77f478c97255ad4fe4fe1730be49b8
Jason Bartz <me@jason-bartz.com>
2025-04-29 11:19:37 -0400

    Delete NextGen Site directory

--- NextGen Site/js/script.js
+++
@@ -1,6 +0,0 @@
-const shimmer = document.getElementById('shimmer');
-
-document.addEventListener('mousemove', e => {
-  const { clientX: x, clientY: y } = e;
-  shimmer.style.backgroundPosition = `${x/5}px ${y/5}px`;
-});
